e22b82f3af59887c619c0101539533fdc352d0da
Aditya Narayanan <aditya.narayanan26@gmail.com>
2021-05-19 15:53:28 -0700

    Added simulation function which uses model

--- Monte-Carlo/predict_insomnia.py
+++ Monte-Carlo/predict_insomnia.py
@@ -42,3 +42,44 @@ class Insomnia:
             working_hours -= 1
 
         return coffee_threshold == 0
+
+    def simulate(self, runs=10000):
+        '''
+        Running 10,000 simulations (default) and plotting the resultsOnly highlighting here the parameters not mentioned in the model function
+
+        :param runs: number of times the model will run
+
+        '''
+
+        results = []
+        num_simulations = 0
+
+        while num_simulations < runs:
+            model = self.insomnia_model(self.WORKING_HOURS, self.COFFEE_THRESHOLD, self.P_COUCH, self.P_PJS, self.P_TIRED)
+            results.append(model)
+            num_simulations += 1
+
+        results = list(map(lambda x: 'insomnia' if x else 'sleep', results))
+        results_df = pd.DataFrame(results)
+        results_df.columns = ['type']
+
+        p_insomnia = round(
+            (results_df[results_df['type'] == 'insomnia'].count().values[0])/float(runs), 2)
+        p_sleep = round(
+            (results_df[results_df['type'] == 'sleep'].count().values[0]) / float(runs), 2)
+        print('Probability of having insomnia = ' + str(p_insomnia) +
+              ' || Probability of a good night\'s sleep = ' + str(p_sleep))
+        print(type(results_df['type'][0]))
+
+        # plotting the output of all model runs
+        fig, ax = plt.subplots(figsize=(12, 7), constrained_layout=True)
+        # removing to and right border
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)  # setting the color palette
+        sns.set_palette("Set2")
+        results_df['type'].value_counts().plot(kind='bar', color = ['orange', 'blue'])
+        ax.xaxis.label.set_visible(False)
+        plt.show()
+
+insomnia_sim = Insomnia()
+insomnia_sim.simulate()
